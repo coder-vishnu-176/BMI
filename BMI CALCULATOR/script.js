f06270cbda316c1cb862931647eb303e50bd99af
VISHNU VARDHAN N.M <vishnuvardhann.m176@gmail.com>
2025-12-25 12:10:38 +0530

    Enhance BMI status messages with emojis

--- BMI CALCULATOR/script.js
+++ BMI CALCULATOR/script.js
@@ -10,12 +10,15 @@ function call(){
         document.getElementById("status").innerText="Status: UnderWeight"
     }
     else if(parseFloat(bmi.toFixed(2))>18.5 && bmi.toFixed(2)<24.9 ){
-        document.getElementById("status").innerText="Status: Normal"
+        document.getElementById("status").innerText="Status: Normal ✅"
     }
     else if(parseFloat(bmi.toFixed(2))>25 && bmi.toFixed(2)<29.9 ){
-        document.getElementById("status").innerText="Status: OverWeight"
+        document.getElementById("status").innerText="Status: OverWeight ⚠️"
+    }
+    else if(parseFloat(bmi.toFixed(2))>25){
+        document.getElementById("status").innerText="Status: Obese ❌"
     }
     else{
-        document.getElementById("status").innerText="Status: Obese"
+         document.getElementById("status").innerText="Invalid Input"
     }
 }
